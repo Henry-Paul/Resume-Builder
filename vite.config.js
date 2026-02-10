f920eb258e9a843653521cd187df90e6d62ce9c9
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2026-02-10 23:38:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This must match your GitHub repository name exactly
+  // Case-sensitive! Matches your repo: Resume-Builder
   base: "/Resume-Builder/",
 })
